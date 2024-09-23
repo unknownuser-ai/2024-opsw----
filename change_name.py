dc7e6ec169751fb2451021020cdda2101211d6c3
unknown <cdkth5654@gmail.com>
2024-09-24 00:13:25 +0900

    이름만 수정함

---
+++ change_name.py
@@ -0,0 +1,13 @@
+fruits = {
+    'apple': 1000,
+    'banana': 2000,
+    'plum': 500,
+    'peach': 4000
+}
+
+fruit = input("어서옵쇼~ 어떤 과일을 찾으세요? ")
+
+if fruit in fruits:
+    print(f"아! {fruit}는 {fruits[fruit]}원입죠~")
+else:
+    print(f"아이고~ {fruit}는 매장에 없네요.")
